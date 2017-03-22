73bc8885bdd4f653acc15ea0afa80dca20c7bbe0
florian weigl <fweigl@florians-mbp-2.asv.local>
2017-03-22 13:16:32 +0100

    1

---
+++ app/src/main/java/com/example/prefthrowaway/MyPrefFragment.java
@@ -0,0 +1,36 @@
+package com.example.prefthrowaway;
+
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceGroup;
+import android.widget.Toast;
+
+public class MyPrefFragment extends PreferenceFragment {
+    @Override
+    public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
+
+        addPreferencesFromResource(R.xml.prefs);
+
+        PreferenceGroup preferenceGroup = (PreferenceGroup) findPreference("pref_cat_1");
+
+        SwitchPreference switchPreference = new SwitchPreference(getActivity());
+        switchPreference.setTitle("new");
+        switchPreference.setChecked(true);
+        switchPreference.setDefaultValue(true);
+
+        switchPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(final Preference preference, final Object newValue) {
+
+                Toast.makeText(getActivity(), newValue.toString(), Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
+
+        preferenceGroup.addPreference(switchPreference);
+
+    }
+}
